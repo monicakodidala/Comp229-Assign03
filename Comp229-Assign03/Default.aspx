d3328fd739035ef451b8fa109858bb86f100d08d
monica <monica@MONICA>
2016-11-15 23:04:57 -0500

    Add project files.

---
+++ Comp229-Assign03/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Comp229_Assign03._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+   <asp:Repeater ID="myRepeater" runat="server">
+       <HeaderTemplate>
+           <h1>Employee Name</h1>
+       </HeaderTemplate>
+       <ItemTemplate>
+           <asp:Label ID="lblID" runat="server" Text='<%# Eval("LastName") %>' ></asp:Label>
+           <asp:HyperLink runat="server"
+               NavigateUrl='<%# Eval("StudentID","~/SingleStudent.aspx?id=(0)")%>'>
+                 </asp:HyperLink>
+       </ItemTemplate>
+   </asp:Repeater>
+
+</asp:Content>
---
+++ Comp229-Assign03/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Comp229_Assign03
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Server = MONICA\\MONICASQLEXPRESS;Database = Comp229Assign03;Integrated Security=True");
+            SqlCommand comm = new SqlCommand("SELECT * FROM Students", connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+
+                myRepeater.DataSource = reader;
+                myRepeater.DataBind();
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+} 
+    
